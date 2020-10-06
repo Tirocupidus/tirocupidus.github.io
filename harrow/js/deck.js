bc97d9f001856bd8f5d07be65ca4c95c8c80703f
Tirocupidus <tirocupidus@gmail.com>
2020-10-06 11:11:53 -0500

    Harrow initialization

---
+++ harrow/js/deck.js
@@ -0,0 +1,48 @@
+var cards = [
+  {
+    name: "The Paladin",
+    alignment: "Lawful good",
+    ability: "Strength",
+    description: "symbolizes standing strong in the face of adversity. The Paladin does not back down under any circumstances. This card usually indicates the need to stay the course or do what one knows is right, even if it takes a heavy toll. If the card is misaligned, such a course might be foolhardy",
+    image: "paladin.png"
+  },
+  {
+    name: "The Keep",
+    alignment: "Neutral good",
+    ability: "Strength",
+    description: "is a symbol of quiet strength, one that can move when necessary, yet stand firm through the greatest hardship. Those represented by The Keep are not shaken by any force. If The Keep appears misaligned, it can mean giving way to temptation or falling in the face of greater strength.",
+    image: "keep.png"
+  },
+  {
+    name: "The Big Sky",
+    alignment: "Chaotic good",
+    ability: "Strength",
+    description: "shows an epic moment as the slaves of a nation are freed. The slaves’ freedom specifies momentous and powerful change, as old shackles are cast off in the light of day. If misaligned, those castoff shackles might be replaced with worse ones.",
+    image: "big-sky.png"
+  },
+  {
+    name: "The Forge",
+    alignment: "Lawful neutral",
+    ability: "Strength",
+    description: "evokes strength through great diversity. The blacksmith represents those who can survive the mephits’ trial by fire, but The Forge’s fire is so strong it burns many to cinders instead. This card often represents a dangerous event that needs many sources of strength to overcome.",
+    image: "forge.png"
+  },
+  {
+    name: "The Empty Throne",
+    alignment: "Lawful good",
+    ability: "Charisma",
+    description: "has a sense of loss that is palpable. The ghost signifies that those who are gone will always be with us. They taught us important lessons, if only we choose to listen. This card can bring information from a far-off or ancient source. If misaligned, the ghosts of the past are restless, and might require effort to set at peace.",
+    image: "empty-throne.png"
+  }
+]
+
+/*
+,
+{
+  name: "",
+  alignment: "",
+  ability: "",
+  description: "",
+  image: ""
+}
+*/
---
+++ harrow/js/script.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById("selChoosing").onchange = doChoosing
+  document.getElementById("selSpread").onchange = doSpread
+})
+
+const clearTableau = async() => {
+  document.getElementById("tableau").innerHTML = ""
+}
+
+const doChoosing = async(e) => {
+  clearTableau()
+
+  // Get selected ability then reset control
+  var ability = e.target.value
+  e.target.options[0].selected = true
+
+  // Build choosing deck
+  var choosingDeck = []
+  for (i = 0; i < cards.length; i++) {
+    var card = cards[i]
+    if (card.ability == ability) { choosingDeck.push(card) }
+  }
+
+  // Shuffle and deal
+  shuffle(choosingDeck)
+  for (i = 0; i < choosingDeck.length; i++) {
+    await sleep(100)
+    dealCard(choosingDeck[i], true)
+  }
+}
+
+const doSpread = async(e) => {
+  clearTableau()
+
+  // Get selected pattern then reset control
+  var pattern = e.target.value
+  e.target.options[0].selected = true
+
+  // Shuffle and determine pattern
+  var deck = cards
+  shuffle(deck)
+
+  var rows;
+  var cols;
+  switch(pattern) {
+    case "1x3":
+      rows = 1
+      cols = 3
+      break
+    case "3x3":
+      rows = 3
+      cols = 3
+      break
+  }
+
+  // Deal
+  var cardCount = rows * cols;
+  for (var i = 0; i < cardCount; i++) {
+    await sleep(100)
+    if (i >= deck.length) { break }
+    dealCard(deck[i], i % cols)
+  }
+}
+
+const dealCard = (card, continuedRow) => {
+  var img = document.createElement("img")
+  img.classList.add("card")
+  if (!continuedRow) { img.classList.add("new-row") }
+  img.src = "images/card-back.png"
+  img.id = card.image
+  img.onclick = (e) => {
+    if(e.target.src.indexOf("card-back") != -1) {
+      e.target.src = "images/" + e.target.id
+    } else {
+      e.target.src = "images/card-back.png"
+    }
+  }
+  tableau.appendChild(img)
+}
+
+const shuffle = async(deck) => {
+  deck.sort(() => Math.random() - 0.5)
+}
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
